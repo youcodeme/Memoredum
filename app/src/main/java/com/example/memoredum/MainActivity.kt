44a5a6f368e84a16cc529fbcb2d21a6335a1d848
赵明宇 <tony@gmail.com>
2022-05-02 19:22:35 +0800

    Initial commit

---
+++ app/src/main/java/com/example/memoredum/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.memoredum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
